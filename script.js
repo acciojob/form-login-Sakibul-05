e099b5adf5452d9c2f893b36cbba68fef9447a4d
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-06-01 11:00:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	const firstName = document.getElementById("firstName").value;
+	const lastName = document.getElementById("lastName").value;
+ 
+	return alert(`${firstName} ${lastName}`);
+	
 }
